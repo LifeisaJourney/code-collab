0ca91ad2f7382b35d5228dee84a7b17b2c3c7df0
AaronCCWong <aarn.wong@gmail.com>
2019-09-27 00:57:56 -0400

    Add message receiver

--- src/App.js
+++ src/App.js
@@ -12,10 +12,21 @@ const App = () => {
     useEffect(() => {
         const clientId = uuidv1();
         socket.emit('join', clientId);
+        socket.on('message', handleMessageReceived);
    });
+
+    const handleMessageReceived = (message) => {
+        console.log(message);
+    };
+
+    const handleClick = () => {
+        socket.emit('message', 'Test!');
+    };
 
   return (
     <div className="App">
+      <button onClick={handleClick}>Click</button>
+
       <div className="numerical-lines">
         <h2><Listing/></h2>
       </div>
