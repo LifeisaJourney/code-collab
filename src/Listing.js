b92859ad35e882ccb26f145085faee5cde5d732f
LifeH <hwie.zhu@gmail.com>
2019-09-27 01:53:41 -0400

    listing continuous input

--- src/Listing.js
+++ src/Listing.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
 
 export default class Listing extends Component {
+  updateInput = (lineNumber) => {
+    return (inputValue) => {
+      this.props.updateLine(lineNumber, inputValue)
+    }
+  };
   render() {
     const elements = [1,2,3,4,5,6,7];
     const items = [];
   
       for (const [index, value] of elements.entries()){
         items.push(
-        <li key={index}>{value}.</li>
+            <li key={index}>{value}.
+              <input 
+                onChange= {this.updateInput(value)}
+                value= {this.props.lineState[value]}
+                className = "input-field">
+              </input>
+            </li>
         )}
-
   return (
     <div>
       {items}
